689ab3e564b0cc4cd403d137f42f0dd4734c52eb
Fox <foxse@bk.ru>
2017-06-16 19:33:40 +0300

    save homework

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -20,7 +20,8 @@ namespace TestApp
 			var excelApp = new Excel.Application();
 			excelApp.Visible = false;
 			excelApp.DisplayAlerts = false;
-			excelApp.Workbooks.Open((file == null)? @"D:\All_Work\Intergraph\VNP\Catalog\Templates\test.xlsx" : file);
+			//excelApp.Workbooks.Open((file == null)? @"D:\All_Work\Intergraph\VNP\Catalog\Templates\test.xlsx" : file);
+			excelApp.Workbooks.Open((file == null)? @"C:\Users\fox\Desktop\VnipineftCodeLists.xls" : file);
 			//			XlsDocument xls = new XlsDocument(@"D:\All_Work\Intergraph\VNP\Catalog\Templates\VNIPINEFT_AllCodeLists.xlsx");
 
 
